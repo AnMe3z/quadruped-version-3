149d24f6667225f8ad72742168bdcf4039af86f3
andrey-shefa <andrey-shefa@begachka2.hustle>
2024-01-15 11:21:05 +0200

    cat servo inner loop pwm interrupt (untested)

--- software/leg-controller/build/CMakeFiles/pwm_control.dir/main.c.obj.d
+++ software/leg-controller/build/CMakeFiles/pwm_control.dir/main.c.obj.d
@@ -1,16 +1,28 @@
 CMakeFiles/pwm_control.dir/main.c.obj: \
  /home/andrey-shefa/Documents/repos/quadruped-version-3/software/leg-controller/main.c \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_stdlib/include/pico/stdlib.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
  /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdbool.h \
- /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
- /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
- /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
- /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/assert.h \
  /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
  /home/andrey-shefa/Documents/repos/quadruped-version-3/software/leg-controller/build/generated/pico_base/pico/version.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
  /home/andrey-shefa/Documents/repos/quadruped-version-3/software/leg-controller/build/generated/pico_base/pico/config_autogen.h \
@@ -20,8 +32,6 @@ CMakeFiles/pwm_control.dir/main.c.obj: \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
- /usr/include/newlib/sys/cdefs.h \
- /usr/include/newlib/machine/_default_types.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
  /home/andrey-shefa/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
@@ -45,17 +55,10 @@ CMakeFiles/pwm_control.dir/main.c.obj: \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/uart.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
- /usr/include/newlib/stdio.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdarg.h \
- /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
- /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
- /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
- /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
- /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
- /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
- /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
- /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
- /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2_common/hardware_pwm/include/hardware/pwm.h \
  /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pwm.h \
- /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pwm.h
+ /home/andrey-shefa/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pwm.h \
+ /home/andrey-shefa/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info.h \
+ /home/andrey-shefa/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/defs.h \
+ /home/andrey-shefa/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/structure.h \
+ /home/andrey-shefa/pico/pico-sdk/src/common/pico_binary_info/include/pico/binary_info/code.h
---
+++ software/leg-controller/irqpwm.c
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// Fade an LED between low and high brightness. An interrupt handler updates
+// the PWM slice's output level each time the counter wraps.
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
+
+#define PICO_DEFAULT_LED_PIN 25
+
+void on_pwm_wrap() {
+    printf("interrupt \n");
+}
+
+
+int main() {
+    stdio_init_all();
+	
+    // Tell the LED pin that the PWM is in charge of its value.
+    gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
+    // Figure out which slice we just connected to the LED pin
+    uint slice_num = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
+
+    pwm_clear_irq(slice_num);
+    pwm_set_irq_enabled(slice_num, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 4.f);
+    pwm_init(slice_num, &config, true);
+
+    // Everything after this point happens in the PWM interrupt handler, so we
+    // can twiddle our thumbs
+    while (1)
+        tight_loop_contents();
+}
