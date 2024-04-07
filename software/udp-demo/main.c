1743b5eb25f027f41a381aa7c645677ada8a7bb7
AnMe3z <prodeadlight@gmail.com>
2024-04-07 22:13:04 +0300

    working udp code with commands

--- software/udp-demo/main.c
+++ software/udp-demo/main.c
@@ -14,16 +14,73 @@
 #define BEACON_TARGET "255.255.255.255"
 #define BEACON_INTERVAL_MS 1000
 
-void udp_receive_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port) {
-    if (p != NULL) {
-        // Received packet buffer 'p' is not NULL
-        // You can process the packet here
-        char *packet_data = (char *)p->payload;
-        printf("Received UDP message: %s\n", packet_data);
+#define DEVICE_ID 0 // or 1
 
-        // Don't forget to free the packet buffer
-        pbuf_free(p);
-    }
+int led;
+
+int commands[18];
+
+struct instruction {
+        int ff;
+        int fk;
+        int bf;
+        int bk;
+};
+struct instruction inst;
+
+void process_data(){
+        if (DEVICE_ID == 0) {
+                inst.ff = 10*commands[1] + commands[2];
+                inst.fk = 10*commands[3] + commands[4];
+                inst.bf = 10*commands[5] + commands[6];
+                inst.bk = 10*commands[7] + commands[8];
+                printf("inst.ff %d ", inst.ff);
+                printf("inst.fk %d ", inst.fk);
+                printf("inst.bf %d ", inst.bf);
+                printf("inst.bk %d ", inst.bk);
+        }
+        else {
+                inst.ff = 10*commands[10] + commands[11];
+                inst.fk = 10*commands[12] + commands[12];
+                inst.bf = 10*commands[13] + commands[13];
+                inst.bk = 10*commands[14] + commands[15];
+                printf("inst.ff %d ", inst.ff);
+                printf("inst.fk %d ", inst.fk);
+                printf("inst.bf %d ", inst.bf);
+                printf("inst.bk %d ", inst.bk);
+        }
+}
+
+void udp_receive_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port) {
+        if (p != NULL) {
+                // Received packet buffer 'p' is not NULL
+                // You can process the packet here
+                char *packet_data = (char *)p->payload;
+                printf("Received UDP message: %s\n", packet_data);
+                
+                if (strlen(packet_data) == 36) {
+                        int i;
+                        
+                        while (packet_data[i] != '\0' && packet_data[i+1] != '\0') {
+                                commands[i/2] = ((packet_data[i] - '0') * 10 + (packet_data[i+1] - '0')) - 30;
+                                printf("%d\n", commands[i/2]);
+                                i += 2;
+                        }
+                        
+                        process_data();
+                }
+                else {
+                        printf("ERROR: Message is not 36 characters long\n");
+                }
+
+                //return 0;
+                
+                led = led ^ 1;
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led);
+
+                // Don't forget to free the packet buffer
+                pbuf_free(p);
+        }
 }
 
 int main() {
@@ -42,6 +99,8 @@ int main() {
         return 1;
     } else {
         printf("Connected.\n");
+        led = led ^ 1;
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led);
     }
 
     struct udp_pcb *udp_server_pcb = udp_new();
